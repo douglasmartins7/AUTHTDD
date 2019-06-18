eacab87a47b2e4bf869bc0531019be236d9b2eba
Douglas QA <douglas.santos@avecbrasil.com.br>
2019-06-18 20:25:18 -0300

    update

--- src/routes.js
+++ src/routes.js
@@ -1,4 +1,11 @@
 const routes = require("express").Router();
+const { User } = require("./app/models");
+
+User.create({
+    name: "Diego",
+    email: "diego@rocketseat.com.br",
+    password_hash: "654654654654654654"
+});
 
  // Definição rotas
 
