8dff30efa5d022a732797442fa36f3bf4fd8a702
Douglas <douglasmartinssantoss@gmail.com>
2019-06-21 14:21:03 -0300

    explanation test integration

--- __tests__/integration/session.test.js
+++ __tests__/integration/session.test.js
@@ -2,3 +2,29 @@
     Testam funcionalidades que podem fazer chamadas a API, cadastro no banco de dados, funções que não são puras elas
     possuem efeitos colaterais.
 */
+
+
+/*
+    describe("Authentication", () => {
+        it("should sum two numbers", () => {
+            const x = 2;
+            const y = 4;
+
+            const sum = x + y;
+
+            expect(sum).toBe(7);
+        });
+    });
+   
+
+    expected 7
+    received 6
+
+    O usuario estava esperando 7 mais recebeu o valor 6
+    ele vai falar onde nosso teste está falhando.
+
+*/
+
+it('', () => {
+
+});
